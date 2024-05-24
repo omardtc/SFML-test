86c12e6d6f04331c477b7ef46f347b176f46cec4
dani24gh <19047@portalcedi.edu.mx>
2024-05-24 14:54:37 -0600

    salto_rango

--- main.cpp
+++ main.cpp
@@ -19,8 +19,8 @@ int main()
     //cout << "Introduzca su nombre" << endl;
     //cin >> name;
 
-    int random = -360+rand()%440;
 
+    int random = 645 - (rand() % 150);
     Texture kirbyTexture;
     if(!kirbyTexture.loadFromFile("images/kirby2dnb.png"))
     {
@@ -31,6 +31,7 @@ int main()
     Sprite kirby;
     kirby.setTexture(kirbyTexture);
     kirby.setTextureRect(IntRect(0,0, 200, 160));
+    //kirby.setScale(0.5f, 0.5f); para reducir tamaño
     int opacidad = 255;
     kirby.setPosition(0,645);
     Player p(name, kirby);
@@ -45,6 +46,8 @@ int main()
     Sprite meta;
     meta.setTexture(metaTexture);
     meta.setPosition(400, random);
+    meta.setScale(0.6f, 0.6f); //para reducir el tamaño
+
     Enemigo enemy1(meta);
 
     while (window.isOpen())
@@ -54,9 +57,9 @@ int main()
         {
             if (event.type == Event::Closed)
                 window.close();
-            if (event.type == Event::MouseButtonPressed)
+            if (event.type == Event::KeyPressed)
             {
-                if (event.mouseButton.button == Mouse::Left)
+                if (event.mouseButton.button == Keyboard::Space)
                 {
                     p.click();
                 }
@@ -71,8 +74,12 @@ int main()
         grid.drawTo(window);
         p.drawTo(window);
         enemy1.drawTo(window);
+
         window.display();
     }
 
     return 0;
 }
+
+
+  
